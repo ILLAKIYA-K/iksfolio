3d77326bcc786746a56d6f581592d92b557160fc
ILLAKIYA-K <125647934+ILLAKIYA-K@users.noreply.github.com>
2023-07-10 18:17:25 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,9 +8,13 @@ const btntwo = document.querySelector('.btn2');
 
 btnone.addEventListener('click',function(){
     mainnav.classList.remove('hidden');
+    document.querySelector('.btnse').style.color = '#886ee8';
+    
     
 })
 
 btntwo.addEventListener('click',function(){
   mainnav.classList.add('hidden');
+    document.querySelector('.btnse').style.color = '#000';
+    
 })
